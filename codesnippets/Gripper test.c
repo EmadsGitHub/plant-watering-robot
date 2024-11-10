8e94e6588e33565f7a8d6d6951681e2fc2722976
Taha Malik <162917044+Taha-M567@users.noreply.github.com>
2024-11-10 16:45:12 -0500

    Update Gripper test.c

--- codesnippets/Gripper test.c
+++ codesnippets/Gripper test.c
@@ -1,23 +1,30 @@
 #include "EV3Servo-lib-UW.c"
-
 // Motor locations on interface board
 const int SV_CSERVO1 = 1;
 const int SV_CSERVO2 = 2;
 const int SV_STANDARD = 3;
 const int SV_GRIPPER = 4;
-
 // Demo types
 const int CONTINUOUS = buttonUp;
 const int GRIPPER = buttonEnter;
 const int STANDARD = buttonDown;
 
-task main()
-{
-setServoSpeed(S1,SV_CSERVO1, 0);
-wait1Msec(1000);
-setServoSpeed(S1,SV_CSERVO1, 60);
-wait1Msec(2000);
-setServoSpeed(S1,SV_CSERVO1, -60);
+void Gripper(){
+	setServoSpeed(S4,SV_CSERVO1, 0);
+	wait1Msec(1000);
+	setServoSpeed(S4,SV_CSERVO1, 60);
+	wait1Msec(2000);
+	setServoSpeed(S4,SV_CSERVO1, -60);
+}
+
+void GripperClose(){
+	setServoSpeed(S4,SV_CSERVO1, -60);
+}
+void GripperOpen(){
+	setServoSpeed(S4,SC_CSERVO1, 60;)
+}
 
 
+task main(){
+	
 }
