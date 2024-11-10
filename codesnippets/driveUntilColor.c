57bc74722b6da7d8e51e7bae6662d336a860a111
Taha Malik <162917044+Taha-M567@users.noreply.github.com>
2024-11-10 16:44:17 -0500

    Update driveUntilColor.c

--- codesnippets/driveUntilColor.c
+++ codesnippets/driveUntilColor.c
@@ -1,6 +1,4 @@
 /* void waterPlantBasedOnColor(*colorDetected) {
-
-
     if (colorDetected == (int)colorRed) {
         motor[motorC] = 50;
         wait1Msec(1000);
@@ -31,7 +29,6 @@
       	wait1Msec(7000);
       	motor[motorC] = 0;
     }
-
     else {
         motor[motorC] = 0;
     }
@@ -45,16 +42,15 @@ void orderList (int plantOrder[], int size, int runtime){
 		for (int j = 0; j<size; j++){
 			wateringOrder[(i*size)+j]=plantOrder[j];
 		}
-
 	}
 	for (int m = 0; m<size*cycles; m++){
 		cout<<wateringOrder[m]<<endl;
 	}
 }
 */
-
 void driveUntilColor(int colorValue) {
-/*  0 = No Colour
+/*  
+0 = No Colour
 1 = Black
 2 = Blue
 3 = Green
@@ -62,28 +58,34 @@ void driveUntilColor(int colorValue) {
 5 = Red
 6 = White
 7 = Brown
-    */
+*/
     motor[motorA]=29;
     motor[motorD] = 25;
-    while(SensorValue[S4]!= 3){}
-
-    motor[motorA]=motor[motorD] = 0;
+    while(SensorValue[S4]!= colorValue){}
+    motor[motorA] = motor[motorD] = 0;
+    
+    motor[motorA] = -29;
+    motor[motorD] = -25;
+    wait1Msec(500)///500 is a placeholder, do the math to actually find out how long it goes back for
+    motor[motorA] = motor[motorD] = 0;
+    resetGyro[S1];
+    
+    motor[motorA] = -29;
+    motor[motorD] = 25;
+    while(abs(getGyroDegrees[S1] < 90)){}
+    motor[motorA] = motor[motorD] = 0;
+    
+    motor[motorA] = 29;
+    motor[motorD] = 25;
+    wait1Msec(50); //50 is a placeholder, needs trial & error, or motor encoder math
+    motor[motorA] = motor[motorD] = 0;
 }
-
 task main() {
-
 		SensorType[S4] = sensorEV3_Color;
 		wait1Msec(50);
 		SensorMode[S4] = modeEV3Color_Color;
 		wait1Msec(50);
-
-
     int targetColors[6] = {1, 2, 3, 4, 5, 6};
-
     driveUntilColor(1);
-
     displayTextLine(0, "Color Sensor Initialized");
-
-
-
 }
