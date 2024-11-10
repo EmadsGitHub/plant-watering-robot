47914ea42e3c94c524b979bc0ca653a3cf35db7c
Emad Rahman <emad__rahman@hotmail.com>
2024-11-10 17:31:01 -0500

    Reverted

--- codesnippets/timeTest.c
+++ codesnippets/timeTest.c
@@ -1,70 +1,18 @@
+#pragma DebuggerWindows ("Globals")
 int days = 0;
 int hours = 0;
 int minutes = 0;
-int setTime() {
-    int currentSelection = 0;
-    bool finished = false;
-    while(!finished) {
-        displayBigTextLine(1, "Set Time:");
-        displayBigTextLine(3, "Days: %d", days);
-        displayBigTextLine(5, "Hours: %d", hours);
-        displayBigTextLine(7, "Minutes: %d", minutes);
-        if (getButtonPress(buttonUp)) {
-            if (currentSelection == 0) days++;
-            else if (currentSelection == 1) hours = (hours + 1) % 24;
-            else if (currentSelection == 2) minutes = (minutes + 1) % 60;
-            while (getButtonPress(buttonUp)) {};
-        } else if (getButtonPress(buttonDown)) {
-            if (currentSelection == 0 && days > 0) days--;
-            else if (currentSelection == 1 && hours > 0) hours--;
-            else if (currentSelection == 2 && minutes > 0) minutes--;
-            while (getButtonPress(buttonDown)) {};
-        }
-        if (getButtonPress(buttonRight)) {
-            currentSelection = (currentSelection + 1) % 3;
-            while (getButtonPress(buttonRight)) {};
-        } else if (getButtonPress(buttonLeft)) {
-            currentSelection = (currentSelection + 2) % 3;
-            while (getButtonPress(buttonLeft)) {};
-        }
-        if (getButtonPress(buttonEnter)) {
-            while (getButtonPress(buttonEnter)) {};
-            finished = true;
-        }
-    }
-    return days*24*60*60 + hours*3600 + minutes * 60;
-}
-int calculateRuntimeInSeconds() {
-    int totalSeconds = 0;
-    totalSeconds += days * 86400;
-    totalSeconds += hours * 3600;
-    totalSeconds += minutes * 60;
-    return totalSeconds;
-}
-void runRobot(int runtimeInSeconds) {
-    clearTimer(T1);
-    motor[motorA] = motor[motorD] = 50;
-    while(time1[T1] < runtimeInSeconds * 1000) {};
-    motor[motorA] = motor[motorD] = 50;
-}
-void orderList (int plantOrder[], int cycles, int &WateringOrder[]){
-	int size = sizeof(plantOrder);
-	for (int i=0; i<cycles; i++){
-		for (int j = 0; j<size; j++){
-			WateringOrder[(i*size)+j]=plantOrder[j];
-		}
-	}
-}
+
 void setDuration(int pos, int *intervalArray) {
 	int mins=0;
-	int hrs=0;
+	int secs=0;
 	bool newplant = false;
 
 	while(!newplant) {
 	    eraseDisplay();
-	    displayBigTextLine(1, "How often?");
-	    displayBigTextLine(3, "Hours (Up)");
-	    displayBigTextLine(5, "Minutes (Down)");
+	    displayBigTextLine(1, "How long?");
+	    displayBigTextLine(3, "Minutes (Up)");
+	    displayBigTextLine(5, "Seconds (Down)");
 	    displayBigTextLine(7, "Done (Other)");
 
 	    // Wait for any button press
@@ -81,20 +29,20 @@ void setDuration(int pos, int *intervalArray) {
 	        while (!getButtonPress(buttonEnter)) {
 	            eraseDisplay();  // Clear display each update
 	            displayBigTextLine(1, "Increase Time");
-	            displayBigTextLine(3, "Hours: %d", hrs);
+	            displayBigTextLine(3, "Mins: %d", mins);
 
 	            if (getButtonPress(buttonUp)) {
 	                while(getButtonPress(buttonUp)) {}
 	                wait1Msec(20);
-	                hrs = hrs + 1;
+	                mins = mins + 1;
 	            }
 	            if (getButtonPress(buttonDown)) {
 	                while(getButtonPress(buttonDown)) {}
 	                wait1Msec(20);
-	                hrs = hrs - 1;
+	                mins = mins - 1;
 
 	                // Add bounds checking
-	                if (hrs < 0) hrs = 0;
+	                if (mins < 0) mins = 0;
 	            }
 	            wait1Msec(100);  // Reduced delay for better responsiveness
 	        }
@@ -111,20 +59,20 @@ void setDuration(int pos, int *intervalArray) {
 	        while (!getButtonPress(buttonEnter)) {
 	            eraseDisplay();  // Clear display each update
 	            displayBigTextLine(1, "Increase Time");
-	            displayBigTextLine(3, "Minutes: %d", mins);
+	            displayBigTextLine(3, "Secs: %d", secs);
 
 	            if (getButtonPress(buttonUp)) {
 	                while(getButtonPress(buttonUp)) {}
 	                wait1Msec(20);
-	                mins = mins + 5;
+	                secs = secs + 5;
 	            }
 	            if (getButtonPress(buttonDown)) {
 	                while(getButtonPress(buttonDown)) {}
 	                wait1Msec(20);
-	                mins = mins - 5;
+	                secs = secs - 5;
 
 	                // Add bounds checking
-	                if (mins < 0) mins = 0;
+	                if (secs < 0) secs = 0;
 	            }
 	            wait1Msec(100);
 	        }
@@ -136,15 +84,143 @@ void setDuration(int pos, int *intervalArray) {
 	        newplant = true;
 	        while (getButtonPress(buttonAny)) {}  // Wait for any button release
 	        wait1Msec(20);
-	        intervalArray[pos] = hrs*3600+mins*60;
+	        intervalArray[pos] = mins*60+secs;
 	    }
 	}
 
 }
+
+void setTime(int *intervalArray) {
+
+    int currentSelection = 0;
+    bool finished = false;
+
+    while(!finished) {
+
+        displayBigTextLine(1, "Set Time:");
+        displayBigTextLine(3, "Days: %d", days);
+        displayBigTextLine(5, "Hours: %d", hours);
+        displayBigTextLine(7, "Minutes: %d", minutes);
+
+        if (getButtonPress(buttonUp)) {
+
+            if (currentSelection == 0) days++;
+            else if (currentSelection == 1) hours = (hours + 1) % 24;
+            else if (currentSelection == 2) minutes = (minutes + 1) % 60;
+
+            while (getButtonPress(buttonUp)) {};
+
+        } else if (getButtonPress(buttonDown)) {
+
+            if (currentSelection == 0 && days > 0) days--;
+            else if (currentSelection == 1 && hours > 0) hours--;
+            else if (currentSelection == 2 && minutes > 0) minutes--;
+
+            while (getButtonPress(buttonDown)) {};
+        }
+
+        if (getButtonPress(buttonRight)) {
+
+            currentSelection = (currentSelection + 1) % 3;
+            while (getButtonPress(buttonRight)) {};
+
+        } else if (getButtonPress(buttonLeft)) {
+
+            currentSelection = (currentSelection + 2) % 3;
+            while (getButtonPress(buttonLeft)) {};
+        }
+        if (getButtonPress(buttonEnter)) {
+        	finished = true;
+
+        }
+    }
+        eraseDisplay();
+				bool willloop = true;
+						while(willloop){
+	            displayBigTextLine(1, "Plant?");
+			        displayBigTextLine(3, "Cactus (Left)");
+			        displayBigTextLine(5, "Jade (Right)");
+			        displayBigTextLine(7, "Aloe (Up)");
+			        displayBigTextLine(9, "Finished (Down)");
+	            while(!getButtonPress(buttonAny)){}
+
+						    if (getButtonPress(buttonLeft)) {
+						        // Wait for button release and add small delay for debouncing
+						     		while (getButtonPress(buttonLeft)){}
+										wait1Msec(20);  // Debounce delay
+										setDuration(0, intervalArray);
+						    }
+						    if (getButtonPress(buttonRight)) {
+						        // Wait for button release and add small delay for debouncing
+						     		while (getButtonPress(buttonRight)){}
+										wait1Msec(20);  // Debounce delay
+										setDuration(1, intervalArray);
+						    }
+						    if (getButtonPress(buttonUp)) {
+						        // Wait for button release and add small delay for debouncing
+						     		while (getButtonPress(buttonUp)){}
+										wait1Msec(20);  // Debounce delay
+										setDuration(2, intervalArray);
+						    }
+						    if (getButtonPress(buttonDown)) {
+						        // Wait for button release and add small delay for debouncing
+						     		while (getButtonPress(buttonDown)){}
+										willloop = false;
+						    }
+						}
+
+
+
+}
+
+
+
+int calculateRuntimeInSeconds() {
+
+    int totalSeconds = 0;
+
+    totalSeconds += days * 86400;
+    totalSeconds += hours * 3600;
+    totalSeconds += minutes * 60;
+
+    return totalSeconds;
+}
+
+void runRobot(int runtimeInSeconds) {
+
+    clearTimer(T1);
+
+    motor[motorA] = motor[motorD] = 50;
+
+    while(time1[T1] < runtimeInSeconds * 1000) {};
+
+    motor[motorA] = motor[motorD] = 50;
+}
+
+/*
+void orderList (int plantOrder[], int size, int runtime){
+	int cycles = runtime/30000;
+	int q = 0;
+	length = size*cycles;
+	int wateringOrder[length];
+	for (int i=0; i<cycles; i++){
+		for (int j = 0; j<size; j++){
+			wateringOrder[(i*size)+j]=plantOrder[j];
+		}
+	}
+}
+
+*/
+
 task main() {
-    setTime();
+    string plantArray[3] = {"Cactus", "Jade", "Aloe"};
+    int intervalArray[3];
+    setTime(intervalArray);
     int runtimeInSeconds = calculateRuntimeInSeconds();
+
+
     displayBigTextLine(9, "Runtime: %d sec", runtimeInSeconds);
     wait1Msec(2000);
+
     runRobot(runtimeInSeconds);
 }
