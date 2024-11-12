34c783c5b30c9f6adf45f56046fbc5e09d89360d
Emad Rahman <emad__rahman@hotmail.com>
2024-11-12 18:00:56 -0500

    Updated waterMotor

--- codesnippets/waterMotor.c
+++ codesnippets/waterMotor.c
@@ -1,46 +1,17 @@
-void waterPlantBasedOnColor(*colorDetected) {
+void waterPlantBasedOnColor(int duration) {
 
-
-    if (colorDetected == (int)colorRed) {
-        motor[motorC] = 100;
-        wait1Msec(2000);
-        motor[motorC] = 0;
-    }
-    else if (colorDetected == (int)colorBlue) {
-        motor[motorC] = 100;
-        wait1Msec(6000);
-        motor[motorC] = 0;
-    }
-    else if (colorDetected == (int)colorGreen) {
-        motor[motorC] = 100;
-        wait1Msec(1000);
-        motor[motorC] = 0;
-    }
-    else if (colorDetected == (int)colorYellow) {
-    		motor[motorC] = 100;
-      	wait1Msec(5000);
-      	motor[motorC] = 0;
-    }
-     else if (colorDetected == (int)colorBlack) {
-    		motor[motorC] = 100;
-      	wait1Msec(3000);
-      	motor[motorC] = 0;
-    }
-     else if (colorDetected == (int)colorBrown) {
-    		motor[motorC] = 100;
-      	wait1Msec(7000);
-      	motor[motorC] = 0;
-    }
-
-    else {
-        motor[motorC] = 0;
-    }
+	motor[motorC]= 20;
+	wait1Msec(700);
+	motor[motorC] = 0;
+	wait1Msec(duration);
+	motor[motorC] = -20;
+	wait1Msec(700);
+	motor[motorC] = 0;
+	
 }
 
 
 task main() {
-    while(true) {
-        waterPlantBasedOnColor();
-        wait1Msec(100);
-    }
+   waterPlantBasedOnColor(2000);
+   wait1Msec(100);
 }
