8dd4bbd152ad456e975a856b6f09b3e06e54733d
Emad Rahman <emad__rahman@hotmail.com>
2024-11-12 20:33:19 -0500

    A whole lotta updates

--- codesnippets/driveUntilColor.c
+++ codesnippets/driveUntilColor.c
@@ -1,55 +1,6 @@
-/* void waterPlantBasedOnColor(*colorDetected) {
-    if (colorDetected == (int)colorRed) {
-        motor[motorC] = 50;
-        wait1Msec(1000);
-        motor[motorC] = 0;
-    }
-    else if (colorDetected == (int)colorBlue) {
-        motor[motorC] = 50;
-        wait1Msec(6000);
-        motor[motorC] = 0;
-    }
-    else if (colorDetected == (int)colorGreen) {
-        motor[motorC] = 50;
-        wait1Msec(1000);
-        motor[motorC] = 0;
-    }
-    else if (colorDetected == (int)colorYellow) {
-    		motor[motorC] = 50;
-      	wait1Msec(5000);
-      	motor[motorC] = 0;
-    }
-     else if (colorDetected == (int)colorBlack) {
-    		motor[motorC] = 50;
-      	wait1Msec(3000);
-      	motor[motorC] = 0;
-    }
-     else if (colorDetected == (int)colorBrown) {
-    		motor[motorC] = 50;
-      	wait1Msec(7000);
-      	motor[motorC] = 0;
-    }
-    else {
-        motor[motorC] = 0;
-    }
-} */
-/*
-void orderList (int plantOrder[], int size, int runtime){
-	int cycles = runtime/30000;
-	int q = 0;
-	int wateringOrder[size*cycles];
-	for (int i=0; i<cycles; i++){
-		for (int j = 0; j<size; j++){
-			wateringOrder[(i*size)+j]=plantOrder[j];
-		}
-	}
-	for (int m = 0; m<size*cycles; m++){
-		cout<<wateringOrder[m]<<endl;
-	}
-}
-*/
+
 void driveUntilColor(int colorValue) {
-/*  
+	/*
 0 = No Colour
 1 = Black
 2 = Blue
@@ -59,33 +10,65 @@ void driveUntilColor(int colorValue) {
 6 = White
 7 = Brown
 */
-    motor[motorA]=29;
-    motor[motorD] = 25;
-    while(SensorValue[S4]!= colorValue){}
-    motor[motorA] = motor[motorD] = 0;
-    
+const float wheelradius=2.75;
+		float CONVERSION = (2*PI*wheelradius)/360;
+		while(SensorValue[S4]!= (int)colorBlack){
+			while (SensorValue[S4]!= colorValue){
+			nMotorEncoder[motorA]=0;
+			motor[motorA]=29;
+    	motor[motorD] = 25;
+    	while (abs(nMotorEncoder)*CONVERSION<200)){}
+    	motor[motorA] = motor[motorD] = 0;
+    	wait1Msec(200);
+    	motor[motorA] = -29;
+    	motor[motorD] = 25;
+    	while(abs(getGyroDegrees[S1] < 90)){}
+    	motor[motorA] = motor[motorD] = 0;
+    	resetGyro[S1];
+    	wait1Msec(200);
+    	nMotorEncoder[motorA]=0;
+			motor[motorA]=29;
+    	motor[motorD] = 25;
+    	while (abs(nMotorEncoder)*CONVERSION<100)){}
+    	motor[motorA] = motor[motorD] = 0;
+    	wait1Msec(200);
+    	motor[motorA] = -29;
+    	motor[motorD] = 25;
+    	while(abs(getGyroDegrees[S1] < 90)){}
+    	motor[motorA] = motor[motorD] = 0;
+    	resetGyro[S1];
+    	wait1Msec(200);
+			}
+
+
+		motor[motorA] = motor[motorD] = 0;
     motor[motorA] = -29;
     motor[motorD] = -25;
     wait1Msec(500)///500 is a placeholder, do the math to actually find out how long it goes back for
     motor[motorA] = motor[motorD] = 0;
     resetGyro[S1];
-    
+
     motor[motorA] = -29;
     motor[motorD] = 25;
     while(abs(getGyroDegrees[S1] < 90)){}
     motor[motorA] = motor[motorD] = 0;
-    
+
     motor[motorA] = 29;
     motor[motorD] = 25;
     wait1Msec(50); //50 is a placeholder, needs trial & error, or motor encoder math
     motor[motorA] = motor[motorD] = 0;
+	}
 }
 task main() {
 		SensorType[S4] = sensorEV3_Color;
 		wait1Msec(50);
 		SensorMode[S4] = modeEV3Color_Color;
 		wait1Msec(50);
-    int targetColors[6] = {1, 2, 3, 4, 5, 6};
+		SensorType[S1] = sensorEV3_Gyro;
+		wait1Msec(50);
+		SensorMode[S1] = modeEV3Gyro_Calibration;
+		wait1Msec(50);
+		SensorMode[S1] = modeEV3Gyro_RateAndAngle;
+		wait1Msec(50);
     driveUntilColor(1);
-    displayTextLine(0, "Color Sensor Initialized");
 }
---
+++ codesnippets/plantGripper.c
@@ -0,0 +1,70 @@
+#include "EV3Servo-lib-UW.c"
+// Motor locations on interface board
+const int SV_CSERVO1 = 1;
+const int SV_CSERVO2 = 2;
+const int SV_STANDARD = 3;
+const int SV_GRIPPER = 4;
+// Demo types
+const int CONTINUOUS = buttonUp;
+const int GRIPPER = buttonEnter;
+const int STANDARD = buttonDown;
+
+void GripperClose(){
+	setServoSpeed(S4,SV_CSERVO1, -60);
+}
+void GripperOpen(){
+	setServoSpeed(S4,SV_CSERVO1, 60);
+}
+
+void plantPickup(){
+	GripperClose();
+	wait1Msec(1000);
+	motor[motorA] = -29;
+	motor[motorD] = 25;
+	while(abs(getGyroDegrees(S1)) < 180){}
+  motor[motorA] = motor[motorD] = 0;
+  resetGyro(S1);
+  motor[motorA] = 29;
+	motor[motorD] = 25;
+	while(SensorValue[S4]!=(int)colorBrown){}
+	motor[motorA] = motor[motorD] = 0;
+	motor[motorA] = -29;
+	motor[motorD] = 25;
+	while(abs(getGyroDegrees(S1)) < 180){}
+  motor[motorA] = motor[motorD] = 0;
+  resetGyro(S1);
+}
+
+void plantDropoff(){
+	GripperOpen();
+	wait1Msec(1000);
+	motor[motorA] = -29;
+	motor[motorD] = 25;
+	while(abs(getGyroDegrees(S1)) < 180){}
+  motor[motorA] = motor[motorD] = 0;
+  resetGyro(S1);
+  motor[motorA] = 29;
+	motor[motorD] = 25;
+	while(SensorValue[S4]!=(int)colorBrown){}
+	motor[motorA] = motor[motorD] = 0;
+	motor[motorA] = -29;
+	motor[motorD] = 25;
+	while(abs(getGyroDegrees(S1)) < 180){}
+  motor[motorA] = motor[motorD] = 0;
+  resetGyro(S1);
+}
+
+
+
+task main(){
+		SensorType[S4] = sensorEV3_Color;
+		wait1Msec(50);
+		SensorMode[S4] = modeEV3Color_Color;
+		wait1Msec(50);
+		SensorType[S1] = sensorEV3_Gyro;
+		wait1Msec(50);
+		SensorMode[S1] = modeEV3Gyro_Calibration;
+		wait1Msec(50);
+		SensorMode[S1] = modeEV3Gyro_RateAndAngle;
+		wait1Msec(50);
+}
