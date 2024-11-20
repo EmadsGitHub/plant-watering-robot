c65b3cee370eabb7378c6f1e59568bca921ec6e3
Emad Rahman <emad__rahman@hotmail.com>
2024-11-20 02:01:30 -0500

    Updated driving again

--- codesnippets/driveUntilColor.c
+++ codesnippets/driveUntilColor.c
@@ -2,7 +2,7 @@
 
 #include "EV3Servo-lib-UW.c";
 const int SV_CSERVO1 = 1;
-
+float CONVERSION = (2*PI*2.75)/360;
 
 
 void GripperClose(){
@@ -18,18 +18,18 @@ void plantDropoff(){
 	GripperOpen();
 	wait1Msec(500);
 	nMotorEncoder[motorA] = 0;
-  motor[motorA] = 25;
+  motor[motorA] = 26;
   motor[motorD] = 25;
   while(abs(nMotorEncoder[motorA])*CONVERSION<5){}
-	motor[motorA] = -15;
-	motor[motorD] = 15;
+	motor[motorA] = -10;
+	motor[motorD] = 10;
 	while(abs(getGyroDegrees(S1)) < 90){}
   motor[motorA] = motor[motorD] = 0;
   resetGyro(S1);
   wait1Msec(50);
 }
 
-void plantPickup(){
+void plantPickup(int plant){
 	float CONVERSION = (2*PI*2.75)/360;
 	GripperClose();
 	wait1Msec(1000);
@@ -39,10 +39,15 @@ void plantPickup(){
 
   motor[motorA] = 25;
   motor[motorD] = 25;
-  while(abs(nMotorEncoder[motorA])*CONVERSION<2){}
-	motor[motorA] = -15;
-	motor[motorD] = 15;
-	while(abs(getGyroDegrees(S1)) < 90){}
+  if (plant == 0){
+  	while(abs(nMotorEncoder[motorA])*CONVERSION<10){}
+  }
+  if (plant == 1){
+  	while(abs(nMotorEncoder[motorA])*CONVERSION<10){}
+	}
+	motor[motorA] = -10;
+	motor[motorD] = 10;
+	while(abs(getGyroDegrees(S1)) < 82){}
   motor[motorA] = motor[motorD] = 0;
   resetGyro(S1);
   wait1Msec(50);
@@ -57,30 +62,96 @@ void driveUntilColor(int plant, int dir) {
 2 = Aloe
 */
 	if (plant == 0){
-			motor[motorA]=-25*dir;
-    	motor[motorD] = -25*dir;
-    	while(SensorValue[S3]<20 || SensorValue[S3]>25){}
+		if (dir==-1){
+			motor[motorA]=26;
+    	motor[motorD] = 25;
+    	while(SensorValue[S3]!=2){}
+    	wait1Msec(50);
+    	while(SensorValue[S3]!=2){}
+    	wait1Msec(50);
+    	while(SensorValue[S3]!=2){}
+    	wait1Msec(50);
     	motor[motorA] = motor[motorD] = 0;
     	wait1Msec(200);
+      nMotorEncoder[motorA]=0;
+    	motor[motorA] = 26;
+  		motor[motorD] = 25;
+  		while(abs(nMotorEncoder[motorA])*CONVERSION<5){}
+  		wait1Msec(50);
+		}
+		else{
+			motor[motorA]=-25;
+    	motor[motorD] = -25;
+    	while(SensorValue[S3]!=2){}
+    	wait1Msec(50);
+    	while(SensorValue[S3]!=2){}
+    	wait1Msec(50);
+    	while(SensorValue[S3]!=2){}
+    	wait1Msec(50);
+    	motor[motorA] = motor[motorD] = 0;
+    	wait1Msec(200);
+
+		}
+
 	}
 	if (plant == 1){
-			motor[motorA]=-25*dir;
-    	motor[motorD] = -25*dir;
-    	while(SensorValue[S3]<20 || SensorValue[S3]>25){}
+			if (dir==-1){
+			motor[motorA]=26;
+    	motor[motorD] = 25;
+    	while(SensorValue[S3]!=6){}
     	motor[motorA] = motor[motorD] = 0;
     	wait1Msec(200);
+      nMotorEncoder[motorA]=0;
+    	motor[motorA] = 26;
+  		motor[motorD] = 25;
+  		while(abs(nMotorEncoder[motorA])*CONVERSION<5){}
+  		wait1Msec(50);
+		}
+		else{
+			motor[motorA]=-25;
+    	motor[motorD] = -25;
+    	while(SensorValue[S3]!=6){}
+    	motor[motorA] = motor[motorD] = 0;
+    	wait1Msec(200);
+
+		}
 	}
 	if (plant == 2){
-			motor[motorA]=-25*dir;
-    	motor[motorD] = -25*dir;
-    	while(SensorValue[S3]<20 || SensorValue[S3]>25){}
+			if (dir==-1){
+			motor[motorA]=26;
+    	motor[motorD] = 25;
+    	while(SensorValue[S3]!=3){}
+    	wait1Msec(50);
+    	while(SensorValue[S3]!=3){}
+    	wait1Msec(50);
+    	while(SensorValue[S3]!=3){}
+    	wait1Msec(50);
     	motor[motorA] = motor[motorD] = 0;
     	wait1Msec(200);
+      nMotorEncoder[motorA]=0;
+    	motor[motorA] = 26;
+  		motor[motorD] = 25;
+  		while(abs(nMotorEncoder[motorA])*CONVERSION<5){}
+  		wait1Msec(50);
+		}
+		else{
+			motor[motorA]=-25;
+    	motor[motorD] = -25;
+    	while(SensorValue[S3]!=3){}
+    	wait1Msec(50);
+    	while(SensorValue[S3]!=3){}
+    	wait1Msec(50);
+    	while(SensorValue[S3]!=3){}
+    	wait1Msec(50);
+    	motor[motorA] = motor[motorD] = 0;
+    	wait1Msec(200);
+
+		}
 	}
 
-    motor[motorA] = (15);
-    motor[motorD] = -15;
-    while(abs(getGyroDegrees(S1)) < 90){}
+    motor[motorA] = (10);
+    motor[motorD] = -10;
+    while(abs(getGyroDegrees(S1)) < 88){}
     motor[motorA] = motor[motorD] = 0;
     resetGyro(S1);
     wait1Msec(50);
@@ -92,7 +163,7 @@ task main() {
 		resetGyro(S1);
 		SensorType[S3] = sensorEV3_Color;
 		wait1Msec(50);
-		SensorMode[S3] = modeEV3Color_Reflected;
+		SensorMode[S3] = modeEV3Color_Color;
 		wait1Msec(50);
 		SensorType[S1] = sensorEV3_Gyro;
 		wait1Msec(50);
@@ -100,7 +171,7 @@ task main() {
 		wait1Msec(50);
 		SensorMode[S1] = modeEV3Gyro_RateAndAngle;
 		wait1Msec(50);
-    driveUntilColor(0, 1);
+    driveUntilColor(2, 1);
     wait1Msec(50);
     nMotorEncoder[motorA] = 0;
     motor[motorA] = -25;
@@ -108,15 +179,20 @@ task main() {
     while(abs(nMotorEncoder[motorA])*CONVERSION<10){}///500 is a placeholder, do the math to actually find out how long it goes back for
     motor[motorA] = motor[motorD] = 0;
     wait1Msec(50);
-    plantPickup();
+    plantPickup(1);
     wait1Msec(50);
     nMotorEncoder[motorA] = 0;
     motor[motorA]= -25;
     motor[motorD]= -25;
-    while(SensorValue[S3]<10 || SensorValue[S3]>18){}
+    while(SensorValue[S3]!=4){}
+    wait1Msec(50);
+    while(SensorValue[S3]!=4){}
+    wait1Msec(50);
+    while(SensorValue[S3]!=4){}
+    wait1Msec(50);
     motor[motorA]=motor[motorD] = 0;
     wait1Msec(50);
-    driveUntilColor(0, -1);
+    driveUntilColor(2, -1);
     wait1Msec(50);
     nMotorEncoder[motorA] = 0;
     motor[motorA] = -25;
@@ -127,7 +203,7 @@ task main() {
     plantDropoff();
     motor[motorA] = 25;
     motor[motorD] = 25;
-    wait1Msec(2000);
+    wait1Msec(1800);
    	motor[motorA] = motor[motorD] = 0;
     wait1Msec(50);
 
